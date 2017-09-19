5907df944fa8a57d4ac9f65baacb1bd8441db865
Sam Reed <sam.rich.reed@berkeley.edu>
2017-09-19 02:06:02 -0700

    Finished

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|b|b.to_i}
+    a.map! {|b|b+2}
+    a.delete_if {|b|b%2!=0}
+    a.uniq!
+    a.delete_if {|b|b>=10}
+    sum=0
+    a.each {|b|sum+=b}
+    sum
   end
 end
 
